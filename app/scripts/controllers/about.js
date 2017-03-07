344082063c1e18bebd4bcff42ebc0976a4e7f73a
Arcades <peret.etienne@gmail.com>
2017-03-07 01:51:25 +0100

    Start

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name twitchApiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the twitchApiApp
+ */
+angular.module('twitchApiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
